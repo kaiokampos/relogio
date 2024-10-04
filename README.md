8c5e3ecce912120afc2e1b9818abf3565c9a3085
Kaio <kaiocamposti@gmail.com>
2024-10-04 13:44:49 -0300

    Projeto relógio finalizado

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">Relógio</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <!-- <a href="#-projeto">Deployment</a> -->
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML5 
+- CSS3
+- JS (POO) 
+
+## 💻 Projeto
+
+O projeto consiste em uma página HTML onde você pode consultar o horario em formato digital e analogico
+
+<!-- ## ☁️ Deployment
+
+Você pode visualizar os deploys do projeto através desses links: -->
+
+
+---
+Desenvolvido por KaioKampos
---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Seleção dos elementos html, para o js
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function updateClock(params) {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+     
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    // -90 por conta do css
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+
+}
+
+function fixZero(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
